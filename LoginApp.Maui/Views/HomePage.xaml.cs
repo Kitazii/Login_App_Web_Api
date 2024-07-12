fb9ccfbd0dc7b60dff30526244104c1bafd2a6e1
mrkie <mrkie@DESKTOP-04STFUU>
2024-07-12 02:14:29 +0100

    Google Maps API Added

--- LoginApp.Maui/Views/HomePage.xaml.cs
+++ LoginApp.Maui/Views/HomePage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Maps;
+using Map = Microsoft.Maui.Controls.Maps.Map;
 namespace LoginApp.Maui.Views;
 
 public partial class HomePage : ContentPage
@@ -5,5 +7,19 @@ public partial class HomePage : ContentPage
 	public HomePage()
 	{
 		InitializeComponent();
-	}
+
+        var location = new Location(55.796391, -4.863235);
+        var mapSpan = new MapSpan(location, 0.01, 0.01);
+        var map = new Map(mapSpan)
+        {
+            MapType = MapType.Street,
+            IsShowingUser = false,
+            IsZoomEnabled = true,
+            IsTrafficEnabled = true,
+            IsScrollEnabled = true
+        };
+
+        // Set the map as the content of the ContentPage
+        Content = map;
+    }
 }
